9a2e6ccb8bd02541f6985bf9d1c218e23c59a7d8
Skipper1284 <sas0367@gmail.com>
2026-06-10 18:45:03 +0300

    Corrections

--- SQL/Запросы/0_Преобразование.sql
+++ SQL/Запросы/0_Преобразование.sql
@@ -1,11 +1,16 @@
-DROP VIEW IF EXISTS "main"."KS_Projects_Modified";
 -- ============================================
 -- ЗАДАЧА 0: Чистка и преобразование таблицы
 -- Создаём очищенное представление (View)
 -- Преобразуем формат Текста в формат Даты в столбцах "launched", "deadline'.
--- Удаляем записи с некорректными датами
+-- Удаляем записи с некорректными или неполными данными.
+-- В результате для представления было отфильтровано 4 429 записи из 323 750
 -- ============================================
+DROP VIEW IF EXISTS "main"."KS_Projects_Modified";
 CREATE VIEW KS_Projects_Modified AS
 	SELECT "ID", "name", "category", "main_category", "currency", DATE("deadline") as "deadline", "goal", DATE("launched") as "launched", "pledged", "state", "backers", "country", "usd pledged"
 	FROM KS_Projects
-	WHERE DATE("launched") IS NOT NULL AND DATE("launched") > "2008-01-01" AND DATE("deadline") IS NOT NULL
+	WHERE DATE("launched") IS NOT NULL 
+	AND DATE("launched") > "2008-01-01" 
+	AND DATE("deadline") IS NOT NULL
+	AND "backers" NOT IN ('failed', 'canceled')
+	AND "country" != 'N,"0'
